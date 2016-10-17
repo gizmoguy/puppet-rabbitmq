5cc28539fe4cec1ddfa3cf50c3b4875f08e148f7
Brad Cowie <brad@gizmoguy.net.nz>
2016-10-18 09:54:45 +1300

    Revert 28fc64a which breaks puppet-apt 1.x support.
    This commit seems to cause dependency cycle, see
    https://tickets.puppetlabs.com/browse/MODULES-2995

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -14,10 +14,7 @@ class rabbitmq::repo::apt(
 
   $pin = $rabbitmq::package_apt_pin
 
-  # ordering / ensure to get the last version of repository
-  Class['rabbitmq::repo::apt']
-  -> Class['apt::update']
-  -> Package<| title == 'rabbitmq-server' |>
+  Class['rabbitmq::repo::apt'] -> Package<| title == 'rabbitmq-server' |>
 
   $ensure_source = $rabbitmq::repos_ensure ? {
     false   => 'absent',
